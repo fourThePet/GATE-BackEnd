d132cbc23210d16c8bb98e9c36788c9eee4b49c4
jungjoo <wjddis000507@gmail.com>
2024-11-15 10:50:49 +0900

    feat : 예외 및 공통응답 처리

---
+++ src/main/java/com/ureca/gate/global/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.ureca.gate.global.exception;
+
+import com.ureca.gate.global.exception.errorcode.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class BusinessException extends RuntimeException{
+    private final ErrorCode errorCode;
+}
