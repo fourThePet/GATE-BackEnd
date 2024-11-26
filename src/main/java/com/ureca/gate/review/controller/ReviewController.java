73e9ff687ef5b97b40d6642924daa7904b0cfe21
rladbstn1000 <614edison@gmail.com>
2024-11-27 03:18:42 +0900

    feat: 리뷰 등록 API, 단건 조회 API

--- src/main/java/com/ureca/gate/review/controller/ReviewController.java
+++ src/main/java/com/ureca/gate/review/controller/ReviewController.java
@@ -39,7 +39,7 @@ public class ReviewController {
     return SuccessResponse.success(
         ReviewResponse.builder()
             .id(id)
-            .starRating(5.0)
+            .starRating(4.0)
             .receipt(true)
             .nickName("뽀삐")
             .placeName("더왈츠 애견카페")
