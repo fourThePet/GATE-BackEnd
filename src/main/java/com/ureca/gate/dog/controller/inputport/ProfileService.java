875a96ef862204fc2e26a433cafb679365ae4d72
hoya <sv05159@naver.com>
2024-11-27 20:06:39 +0900

    feat: 반려견 프로필 수정 API & 리펙토링

--- src/main/java/com/ureca/gate/dog/controller/inputport/ProfileService.java
+++ src/main/java/com/ureca/gate/dog/controller/inputport/ProfileService.java
@@ -3,9 +3,16 @@ package com.ureca.gate.dog.controller.inputport;
 import com.ureca.gate.dog.controller.request.ProfileSaveRequest;
 import com.ureca.gate.dog.domain.Dog;
 import com.ureca.gate.global.util.file.UploadFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface ProfileService {
-    Dog create(Long userId, ProfileSaveRequest profileSaveRequest, UploadFile uploadFile);
-
     Dog getById(Long dogId);
+
+    Dog create(Long userId, ProfileSaveRequest profileSaveRequest, MultipartFile imageFile) throws IOException;
+
+    Dog update(Long userId, Long dogId, ProfileSaveRequest profileSaveRequest, MultipartFile imageFile) throws IOException;
+
+    String imageUrl(Long userId, UploadFile uploadFile);
 }
