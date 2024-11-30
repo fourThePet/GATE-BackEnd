5fe73273f823fc2e76891dc8204a86ca76e802fd
hoya <sv05159@naver.com>
2024-12-01 04:10:52 +0900

    refactor: @Transactional 추가

--- src/main/java/com/ureca/gate/plan/application/PlanServiceImpl.java
+++ src/main/java/com/ureca/gate/plan/application/PlanServiceImpl.java
@@ -6,6 +6,7 @@ import com.ureca.gate.plan.controller.inputport.PlanService;
 import com.ureca.gate.plan.domain.Plan;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -13,7 +14,7 @@ public class PlanServiceImpl implements PlanService {
 
     private final PlanRepository planRepository;
 
-    @Override
+    @Transactional
     public Plan create(PlanCreateCommand planCreateCommand) {
         Plan plan = Plan.from(planCreateCommand);
         return planRepository.save(plan);
