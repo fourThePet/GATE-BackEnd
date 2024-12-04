5bc105a1c7a3365e6ad45a292e3ef27ed7c91ef9
jungjoo <wjddis000507@gmail.com>
2024-12-04 16:11:29 +0900

    docs: [GATE-56] 응답 dto swagger 설명 추가

--- src/main/java/com/ureca/gate/favorites/controller/response/FavoritesResponse.java
+++ src/main/java/com/ureca/gate/favorites/controller/response/FavoritesResponse.java
@@ -1,15 +1,20 @@
 package com.ureca.gate.favorites.controller.response;
 
 import com.ureca.gate.favorites.infrastructure.dto.FavoritesIdWithPlaceDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class FavoritesResponse{
+    @Schema(description = "즐겨찾기 아이디", example = "1")
     private Long favoritesId;
+    @Schema(description = "장소 아이디", example = "1")
     private Long placeId;
+    @Schema(description = "장소 이름", example = "냥냥이카페")
     private String placeName;
+    @Schema(description = "장소 도로명주소", example = "~~~~")
     private String roadAddress;
 
     public static FavoritesResponse from(FavoritesIdWithPlaceDto favoritesIdWithPlaceDto){
