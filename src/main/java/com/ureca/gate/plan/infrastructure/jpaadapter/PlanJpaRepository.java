0119fc9a94e006e742862be83f7275cc30055165
hoya <sv05159@naver.com>
2024-12-05 17:30:58 +0900

    refactor: 쿼리 최적화 - fetch join
    https://www.notion.so/FETCH-JOIN-d10b3a4fc5114e1ab8c96cde7e157177?pvs=4

--- src/main/java/com/ureca/gate/plan/infrastructure/jpaadapter/PlanJpaRepository.java
+++ src/main/java/com/ureca/gate/plan/infrastructure/jpaadapter/PlanJpaRepository.java
@@ -2,6 +2,16 @@ package com.ureca.gate.plan.infrastructure.jpaadapter;
 
 import com.ureca.gate.plan.infrastructure.jpaadapter.entity.PlanEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface PlanJpaRepository extends JpaRepository<PlanEntity, Long>, PlanRepositoryCustom {
+
+    @Query("SELECT p FROM plans p " +
+            "JOIN FETCH p.planPlaces pp " +
+            "JOIN FETCH pp.place pl " +
+            "JOIN FETCH pl.addressEntity.cityEntity " +
+            "WHERE p.id = :planId")
+    Optional<PlanEntity> findById(Long planId);
 }
