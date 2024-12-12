d95336bc81bf56dfca64140fa42d57bf9fb4ebff
hoya <sv05159@naver.com>
2024-12-13 00:50:43 +0900

    fix: 장소 크기 허용 그룹관리

---
+++ src/main/java/com/ureca/gate/place/domain/enumeration/SizeGroup.java
@@ -0,0 +1,23 @@
+package com.ureca.gate.place.domain.enumeration;
+
+import com.ureca.gate.dog.domain.enumeration.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public enum SizeGroup {
+
+    SMALL(List.of("SMALL", "MEDIUM", "LARGE")),
+    MEDIUM(List.of("SMALL", "MEDIUM")),
+    LARGE(List.of("LARGE")),
+    ;
+
+    private final List<String> allowSizeList;
+
+    public static List<String> allowSizesBySize(Size size) {
+        return SizeGroup.valueOf(size.name()).getAllowSizeList();
+    }
+}
