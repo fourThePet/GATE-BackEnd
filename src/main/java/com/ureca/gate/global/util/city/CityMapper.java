ecaf60f4e11d7023c24840cbbeb34d7584a86db3
jungjoo <wjddis000507@gmail.com>
2024-12-17 16:15:56 +0900

    fix : 매핑 지역명 수정

--- src/main/java/com/ureca/gate/global/util/city/CityMapper.java
+++ src/main/java/com/ureca/gate/global/util/city/CityMapper.java
@@ -8,21 +8,21 @@ public class CityMapper {
 
     static {
         cityMap.put(1L, "경기도");
-        cityMap.put(2L, "서울특별시");
-        cityMap.put(3L, "인천광역시");
+        cityMap.put(2L, "서울");
+        cityMap.put(3L, "인천");
         cityMap.put(4L, "경상북도");
-        cityMap.put(5L, "대구광역시");
+        cityMap.put(5L, "대구");
         cityMap.put(6L, "전라북도");
         cityMap.put(7L, "경상남도");
         cityMap.put(8L, "전라남도");
-        cityMap.put(9L, "부산광역시");
-        cityMap.put(10L, "광주광역시");
+        cityMap.put(9L, "부산");
+        cityMap.put(10L, "광주");
         cityMap.put(11L, "강원도");
-        cityMap.put(12L, "대전광역시");
+        cityMap.put(12L, "대전");
         cityMap.put(13L, "충청북도");
-        cityMap.put(14L, "제주특별자치도");
+        cityMap.put(14L, "제주");
         cityMap.put(15L, "충청남도");
-        cityMap.put(16L, "울산광역시");
+        cityMap.put(16L, "울산");
         cityMap.put(17L, "세종");
     }
 
